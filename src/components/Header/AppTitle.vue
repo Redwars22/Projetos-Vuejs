3ea6702a23bb191cd1f606e121a9b3f0aae47965
Redwars22 <andrewpereira.megabyte@gmail.com>
2022-04-21 11:46:38 -0300

    ProjectLegame - versão estável

---
+++ src/components/Header/AppTitle.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="app-title-container">
+    <img
+      src="https://redwars22.github.io/Projetos-Vuejs/resx/legame.png"
+      class="app-logo"
+    />
+    <h1 class="app-title">ProjectLegame v1.0.0</h1>
+    <h1 class="dev-badge" v-if="(isDEV == true)">DEV</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppTitle',
+  data() {
+    return {
+      isDEV: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.dev-badge {
+  color: orange;
+  font-weight: 900;
+  font-size: 25px;
+}
+
+.app-title-container {
+  display: flex;
+  gap: 25px;
+  align-items: center;
+}
+
+.app-logo {
+  width: 25px;
+  height: 25px;
+}
+</style>
---
+++ src/components/Header/AuthorBadge.vue
@@ -0,0 +1,46 @@
+<template>
+  <a
+    href="https://github.com/Redwars22"
+    style="text-decoration: none"
+    target="_blank"
+  >
+    <div class="author-badge">
+      <span>Desenvolvido por</span>
+      <img src="https://github.com/Redwars22.png" class="author-photo" />
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'AuthorBadge',
+};
+</script>
+
+<style scoped>
+.author-badge {
+  background-color: crimson;
+  display: flex;
+  padding: 5px;
+  align-items: center;
+  border-radius: 10px;
+  width: fit-content;
+  font-size: 13px;
+  gap: 10px;
+  color: white;
+  /*transition: background-color 600ms;*/
+  transition: transform 600ms;
+}
+
+.author-badge:hover {
+  background-color: purple;
+  font-weight: bold;
+  transform: rotate(360deg);
+}
+
+.author-photo {
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+}
+</style>
